0c26c195059fc73e11e87bcc9f1f26adb41d5a1d
Theodora Kyriakou <dorothy98@windowslive.com>
2017-02-14 11:22:09 +0200

    Delete ergasia3.py

--- ergasia3.py
+++
@@ -1,9 +0,0 @@
-n = raw_input("Parakalw eisagete arithmous xwrismenous me keno: ")
-numlist = n.split()
-numlist = [int(a) for a in numlist] 
-zeros = numlist.count(0)
-while 0 in numlist:
- numlist.remove(0)
-zerolist = [0] * zeros
-numberlist = numlist + zerolist
-print numberlist
