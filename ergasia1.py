600420ce4328c8a6291484e9e4947c6b82173df3
Dora K <dorothy98@windowslive.com>
2017-02-20 19:31:03 +0200

    Update ergasia1.py

--- ergasia1.py
+++ ergasia1.py
@@ -1,7 +1,15 @@
 given_sentence = raw_input("Dwse thn protash sou: ")
 givenlist = given_sentence.split()
-t = givenlist[-1].count("!")
+lastlist = givenlist[-1]
+newlist = list(lastlist)
+n = 0
+for i in reversed(newlist): 
+ if i == '!':
+  n+=1
+ else:
+  break
 given_sentence = given_sentence.replace('!','')
-given_sentence = given_sentence + "!"*t
-print "H protash pou dothike, mono me ta thaymastika pou vriskontai sto telos, einai h ekshs: "
+given_sentence = given_sentence + "!"*n 
+print "H protash pou dothike, mono me ta thaymastika pou vriskontai sto telos, einai h ekshs: " 
 print given_sentence
+ 
